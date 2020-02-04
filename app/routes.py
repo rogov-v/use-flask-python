9fe591fef78b87ea97d3a8165f11af07527c3fe3
Vadim Rogov <vrogov.exceedteam@gmail.com>
2020-02-04 14:08:34 +0300

    added something

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from app import app
+from flask import render_template, request
 
-@app.route('/')
-@app.route('/index')
+def useIndexForm(firstName, lastName):
+    return "Welcome {0} {1}!!!" .format(firstName, lastName)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
-
-@app.route('/etc')
-def etc():
-    return "new route"
+    if request.method == 'POST':
+        return useIndexForm(request.form['fname'], request.form['lname'])
+    return render_template("index.html")
